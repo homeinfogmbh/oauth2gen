3535eb2228fef4f4a6927895af71d7d8d793b867
Richard Neumann <r.neumann@homeinfo.de>
2022-03-10 12:35:40 +0100

    Update signatures for older API

--- oauth2gen/oauth2/revocation_endpoint.py
+++ oauth2gen/oauth2/revocation_endpoint.py
@@ -3,6 +3,7 @@
 from typing import Iterable, Optional, Type
 
 from authlib.oauth2.rfc7009 import RevocationEndpoint
+from flask import Request
 from peewee import Expression
 
 from oauth2gen.mixins import OAuth2ClientMixin, OAuth2TokenMixin
@@ -26,19 +27,21 @@ class TokenRevocationEndpoint(RevocationEndpoint):
         cls.CLIENT_AUTH_METHODS = list(auth_methods)
 
     def query_token(
-            self, token: str, token_type_hint: str, client: OAuth2TokenMixin
+            self,
+            token: str,
+            token_type_hint: str
     ) -> Optional[OAuth2TokenMixin]:
         """Queries a token from the database."""
         try:
             token = self.TOKEN_MODEL.get(get_token_condition(
-                self.TOKEN_MODEL, client, token, token_type_hint
+                self.TOKEN_MODEL, token, token_type_hint
             ))
         except self.TOKEN_MODEL.DoesNotExist:
             return None
 
         return token
 
-    def revoke_token(self, token: OAuth2ClientMixin) -> None:
+    def revoke_token(self, token: OAuth2ClientMixin, request: Request) -> None:
         """Revokes the respective token."""
         token.revoked = True
         token.save()
@@ -46,19 +49,17 @@ class TokenRevocationEndpoint(RevocationEndpoint):
 
 def get_token_condition(
         model: Type[OAuth2TokenMixin],
-        client: OAuth2ClientMixin,
         token: str,
         token_type_hint: str
 ) -> Expression:
 
-    condition = model.client_id == client.client_id
     condition_access_token = model.access_token == token
     condition_refresh_token = model.refresh_token == token
 
     if token_type_hint == 'access_token':
-        return condition & condition_access_token
+        return condition_access_token
 
     if token_type_hint == 'refresh_token':
-        return condition & condition_refresh_token
+        return condition_refresh_token
 
-    return condition & (condition_access_token | condition_refresh_token)
+    return condition_access_token | condition_refresh_token
